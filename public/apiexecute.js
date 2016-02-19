fed9f18d06f7fb5ce6a5532b4d96935467793fd7
Hrafn Logi Hermannsson <hrafnhe230@nemi.tskoli.is>
2016-02-19 10:27:28 +0000

    Buin ad setja upp graph pie en ekki tengd vid upplysingar

--- public/apiexecute.js
+++ public/apiexecute.js
@@ -1,11 +1,73 @@
 /**
  * Created by 2212962309 on 28.1.2016.
  */
+
 $( "#target" ).submit(function( event ) {
     event.preventDefault();
     executeapiname(this.summonerid.value)
     $("div#contentinfo").css("border-color: red;");
 });
+
+function piefunction(){
+    var salesData=[
+        {label:"Basic",value:1, color:"#3366CC"},
+        {label:"Plus",value:1, color:"#DC3912"},
+        {label:"Lite",value:1, color:"#FF9900"},
+        {label:"Elite",value:1, color:"#109618"},
+        {label:"Delux",value:5, color:"#990099"}
+    ];
+
+    var svg = d3.select("#contentinfo").append("svg").attr("width", 400).attr("height", 400);
+
+    svg.append("g").attr("id","salespie");
+
+    gradPie.draw("salespie", randomData(), 200, 200, 160);
+
+    function randomData(){
+        return salesData.map(function(d){
+            return {label:d.label, value:d.value, color:d.color};});
+    }
+}
+
+
+function executeapichamps(summonerid){
+    if (typeof gloablvar != 'undefined'){
+        clearcontent('contentinfo');
+    }
+
+    var template = $("#leagueasummonername").html();//remove
+
+    var jqxhr3 = $.getJSON("https://eune.api.pvp.net/api/lol/eune/v1.3/stats/by-summoner/"+ summonerid +"/ranked?season=SEASON2016&api_key=e5736e74-4b7a-4470-8fbb-09bc3feb16c2", function(outcome){
+        //console.log(outcome);
+        gloablvar = 'placeholder';
+        apidata = outcome[ summonername ];
+        summonerid = apidata.id;
+        //console.log(apidata);
+        //console.log(summonerid);
+        //console.log(apidata.name);
+    });
+
+    jqxhr3.complete(function() {
+        if (! apidata){
+            alert("Summoner not found!");
+        }
+        else{
+            //console.log(apidata);
+
+            var templateScript = Handlebars.compile(template);
+            //$("#leagueasummonername").html(apidata);
+            var html = templateScript(apidata);
+
+            $("#contentinfo").append(html);
+            var apdata;
+            executeapiinfo(summonerid);
+            summonerid = null;
+        }
+
+    });
+}
+
+
 
 
 function executeapiname(summonername){
@@ -17,15 +79,13 @@ function executeapiname(summonername){
         summonername = summonername.toLowerCase();
         var template = $("#leagueasummonername").html();
         var jqxhr = $.getJSON("https://eune.api.pvp.net/api/lol/eune/v1.4/summoner/by-name/" + summonername + "?api_key=e5736e74-4b7a-4470-8fbb-09bc3feb16c2", function(outcome){
-            console.log(outcome);
+            //console.log(outcome);
             gloablvar = 'placeholder';
-            console.log(summonername);
-            console.log(outcome[ summonername ]);
             apidata = outcome[ summonername ];
             summonerid = apidata.id;
-            console.log(apidata);
-            console.log(summonerid);
-            console.log(apidata.name);
+            //console.log(apidata);
+            //console.log(summonerid);
+            //console.log(apidata.name);
         });
 
         jqxhr.complete(function() {
@@ -33,7 +93,7 @@ function executeapiname(summonername){
                 alert("Summoner not found!");
             }
             else{
-            console.log(apidata);
+            //console.log(apidata);
 
             var templateScript = Handlebars.compile(template);
             //$("#leagueasummonername").html(apidata);
@@ -42,6 +102,7 @@ function executeapiname(summonername){
             $("#contentinfo").append(html);
             var apdata;
             executeapiinfo(summonerid);
+            executeapichamps(summonerid)
             summonerid = null;
             }
 
@@ -53,12 +114,12 @@ function executeapiinfo(summonerid){
     var jqxhr2 = $.getJSON("https://eune.api.pvp.net/api/lol/eune/v1.3/stats/by-summoner/" + summonerid +
         "/summary?season=SEASON3&api_key=e5736e74-4b7a-4470-8fbb-09bc3feb16c2", function(outcome){
         apidata = (outcome);
-        console.log(summonerid);
-        console.log(apidata);
+        //console.log(summonerid);
+        //console.log(apidata);
     });
 
     jqxhr2.complete(function() {
-        console.log(apidata);
+        //console.log(apidata);
 
         var templateScript2 = Handlebars.compile(template2);
         //$("#leagueasummonername").html( apidata);
@@ -76,13 +137,21 @@ function executeapiinfo(summonerid){
 }
 slidercounter = 1
 $( "#prevcontent" ).click(function() {
-    $("#sstats").hide( "slide", { direction: "left"  }, 2000 )
+    $("#sstats").hide( "slide", { direction: "left"  }, 2000, function() {
+        // Use arguments.callee so we don't need a named function
+        //$( this ).prev().hide( "fast", arguments.callee );
+        $("#contentinfo").height($("#prevcontent").height());
+        $("#contentinfo").height($("#nextcontent").height());
+        piefunction();
+    });
     $("#prevcontent").height($("#contentinfo").height());
     $("#nextcontent").height($("#contentinfo").height());
 });
 
 $( "#nextcontent" ).click(function() {
     $("#sstats").show( "slide", { direction: "left"  }, 2000 )
+    $("#prevcontent").height($("#contentinfo").height());
+    $("#nextcontent").height($("#contentinfo").height());
 });
 
 
